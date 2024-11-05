330ded1248c1bfcbdede5739a0bd9bb7353d8d52
hawkeye <173827852+hawkeye1900@users.noreply.github.com>
2024-11-05 11:30:45 +0000

    match/case replaced with if/elif/else ie if 'add' in choice. A slice is used to check only start of choice string

--- main.py
+++ main.py
@@ -16,15 +16,15 @@ def show(arr):
         print("No. Task")
         for i, item in enumerate(arr):
             print(f"{i + 1}: {item.capitalize().strip("\n")}")
+        print("\n")
     else:
         print("Currently no tasks to show\n")
 
 
 todos = check_todos()
-print(todos)
 while True:
-    choice = input("Enter what you want to do from the following options: "
-                   "\nadd"
+    choice = input("Enter one of the following commands followed by a single space: "
+                   "\nadd <enter task>"
                    "\nshow"
                    "\nedit"
                    "\ncomplete"
@@ -32,54 +32,55 @@ while True:
     choice = choice.strip().lower()
 
     # menu options below
-    match choice:
-        case "add":
-            todo = input("Enter a task: ") + "\n"
 
-            with open("files/todos.txt", "r") as file:
-                todos = file.readlines()
+    if "add" in choice[:3]:
+        print(choice[:3])
+        todo = f"{choice[4:]}" + '\n'
 
-            todos.append(todo)
+        with open("files/todos.txt", "r") as file:
+            todos = file.readlines()
 
-            with open("files/todos.txt", "w") as file:
-                file.writelines(todos)
+        todos.append(todo)
 
-        case "show":
-            show(todos)
-        case 'edit':
-            if not todos:
-                print("No tasks to edit\n")
-                continue
-            while True:
-                try:
-                    show(todos)
-                    selected = int(input("Enter the number of the task to edit: ")) - 1
-                    if selected < 0 or selected >= len(todos):
-                        raise IndexError("You must select the number of a task from the list shown.")
-                except (ValueError, IndexError) as e:
-                    if isinstance(e, ValueError):
-                        print("\033[91mINVALID INPUT\033[0m: Please enter a valid number.")
-                    elif isinstance(e, IndexError):
-                        print("\033[91mINVALID INPUT\033[0m: You must select the number of a task from the list shown.")
-                    continue
-                new_task = input("Enter your new task: ")
-                todos[selected] = f"{new_task}\n"
-                print("New task updated successfully")
+        with open("files/todos.txt", "w") as file:
+            file.writelines(todos)
+
+    elif "show" in choice[:4]:
+        show(todos)
+    elif "edit" in choice[:4]:
+        if not todos:
+            print("No tasks to edit\n")
+            continue
+        while True:
+            try:
                 show(todos)
-                with open("files/todos.txt", "w") as file:
-                    file.writelines(todos)
-                break
-        case "complete":
-            print("From the following list of tasks, enter the number of the task to complete")
+                selected = int(input("Enter the number of the task to edit: ")) - 1
+                if selected < 0 or selected >= len(todos):
+                    raise IndexError("You must select the number of a task from the list shown.")
+            except (ValueError, IndexError) as e:
+                if isinstance(e, ValueError):
+                    print("\033[91mINVALID INPUT\033[0m: Please enter a valid number.")
+                elif isinstance(e, IndexError):
+                    print("\033[91mINVALID INPUT\033[0m: You must select the number of a task from the list shown.")
+                continue
+            new_task = input("Enter your new task: ")
+            todos[selected] = f"{new_task}\n"
+            print("New task updated successfully")
             show(todos)
-            number = int(input("Choice: ")) - 1
-            todos.pop(number)
             with open("files/todos.txt", "w") as file:
                 file.writelines(todos)
-            show(todos)
-        case "exit":
             break
-        case _:
-            print("You entered an unknown command")
+    elif "complete" in choice[:9]:
+        print("From the following list of tasks, enter the number of the task to complete")
+        show(todos)
+        number = int(input("Choice: ")) - 1
+        todos.pop(number)
+        with open("files/todos.txt", "w") as file:
+            file.writelines(todos)
+        show(todos)
+    elif "exit" in choice[:4]:
+        break
+    else:
+        print("You entered an unknown command\nMake sure there are no leading spaces\n")
 print("Bye!")
 
